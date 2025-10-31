9011fd519c3d0d47dc6d5179be446766dde9f31e
Swarajmeet Singh <swarajmeet.crebos@gmail.com>
2025-10-31 19:29:32 +0530

    Initial Commit

---
+++ VideoCompressor/VideoCompressorApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoCompressorApp.swift
+//  VideoCompressor
+//
+//  Created by Swarajmeet Singh on 31/10/25.
+//
+
+import SwiftUI
+
+@main
+struct VideoCompressorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
